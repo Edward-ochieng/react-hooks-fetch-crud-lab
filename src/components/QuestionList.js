80e5f2594d94368e1b0d9b2754f1e452f2075350
Edward <edward.ochieng@student.moringaschool.com>
2022-09-27 22:12:31 +0300

    completed lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:4000/questions');
+      const responseData = await response.json();
+      setQuestions(responseData);
+      setIsLoading(false);
+    };
+
+    fetchData()
+  }, [])
+
+  const deleteQuestion = async (id) => {
+    const response = await fetch(`http://localhost:4000/questions/${id}`, { method: 'DELETE', headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } });
+    const responseData = await response.json();
+    console.log(responseData);
+    setQuestions(prevQuestions => prevQuestions.filter((question) => question.id !== id));
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && questions.map((question) => <QuestionItem deleteQuestion={deleteQuestion} key={question.id} question={question}></QuestionItem>)}
+      </ul>
     </section>
   );
 }
